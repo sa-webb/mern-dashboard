f10482cc3e1bfff366d7b5ecd2eb9ff6d2ea0511
Austin Webb <stevenaustinwebb@gmail.com>
2019-06-11 22:45:17 -0400

    added direct route to currently edited component

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import {Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import { NavBar } from './components/NavBar';
+
+import { Home } from './pages/home';
+import { Inventory } from './pages/inventory';
+import { Freight } from './pages/freight';
+import { Create } from './components/inventory/Create';
 
 export const AppRouter = () => {
     return (
         <Router>
             <Switch>
-                <Route exact path="/" component={NavBar} />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/inventory" component={Inventory} />
+                <Route exact path="/freight" component={Freight} />
+                <Route exact path="/inventory/create" component={Inventory} />
             </Switch>
         </Router>
     )
