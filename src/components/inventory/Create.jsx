df16627b80f740fd0e9ac5c380aecaf7c88a1c1e
Austin Webb <stevenaustinwebb@gmail.com>
2019-06-20 11:06:43 -0400

    create basiss

--- src/components/inventory/Create.jsx
+++ src/components/inventory/Create.jsx
@@ -1,62 +1,103 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import React, { Component } from "react";
+import axios from "axios";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import { withStyles } from "@material-ui/styles"
 
 const ranges = [
   {
-    value: '0-20',
-    label: '0 to 20',
+    value: "0-20",
+    label: "0 to 20"
   },
   {
-    value: '21-50',
-    label: '21 to 50',
+    value: "21-50",
+    label: "21 to 50"
   },
   {
-    value: '51-100',
-    label: '51 to 100',
-  },
+    value: "51-100",
+    label: "51 to 100"
+  }
 ];
+
+const styles = {
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  margin: {
+    spacing: 2
+  },
+  textField: {
+    flexBasis: 400
+  }
+}
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
   },
   margin: {
-    margin: theme.spacing(2),
+    margin: theme.spacing(2)
   },
   textField: {
-    flexBasis: 400,
-  },
+    flexBasis: 400
+  }
 }));
 
-export function Create() {
-  const classes = useStyles();
-  const [values, setValues] = React.useState({
-    amount: '',
-    password: '',
-    weight: '',
-    weightRange: '',
-    showPassword: false,
-  });
+class CreateInvoice extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeInvoiceName = this.onChangeInvoiceName.bind(this);
+    this.onChangeInvoiceFull = this.onChangeInvoiceFull.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-  const handleChange = prop => event => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
+    this.state = {
+      name: '',
+      full: '',
+    };
+  }
 
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword });
-  };
+  onChangeInvoiceFull(e) {
+    this.setState({
+        full: e.target.value
+    })
+  }
 
-  return (
-    <div className={classes.root}>
-      <TextField
+  onChangeInvoiceName(e) {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    const newInvoice = {
+      name: this.state.name,
+      full: this.state.full,
+    };
+    axios
+      .post("http://localhost:5000/invoices/add", newInvoice)
+      .then(res => console.log(res.data));
+
+    this.setState({
+      name: '',
+      full: '',
+    });
+  }
+
+  render() {
+    const classes = { useStyles };
+    return (
+      <div className={classes.root}>
+        <form onSubmit={this.onSubmit}>
+          <TextField
         id="outlined-simple-start-adornment"
         className={clsx(classes.margin, classes.textField)}
         variant="outlined"
@@ -65,7 +106,7 @@ export function Create() {
           startAdornment: <InputAdornment position="start">Kg</InputAdornment>,
         }}
       />
-      <TextField
+          {/* <TextField
         select
         className={clsx(classes.margin, classes.textField)}
         variant="outlined"
@@ -81,31 +122,33 @@ export function Create() {
             {option.label}
           </MenuItem>
         ))}
-      </TextField>
-      <TextField
-        id="outlined-adornment-amount"
-        className={clsx(classes.margin, classes.textField)}
-        variant="outlined"
-        label="Amount"
-        value={values.amount}
-        onChange={handleChange('amount')}
-        InputProps={{
-          startAdornment: <InputAdornment position="start">$</InputAdornment>,
-        }}
-      />
-      <TextField
+      </TextField> */}
+          <TextField
+            id="outlined-adornment-amount"
+            className={clsx(classes.margin, classes.textField)}
+            variant="outlined"
+            label="Amount"
+            value={this.state.name}
+            onChange={this.onChangeInvoiceName}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">$</InputAdornment>
+              )
+            }}
+          />
+          <TextField
         id="outlined-adornment-weight"
         className={clsx(classes.margin, classes.textField)}
         variant="outlined"
         label="Weight"
-        value={values.weight}
-        onChange={handleChange('weight')}
+        value={this.state.full}
+        onChange={this.onChangeInvoiceFull}
         helperText="Weight"
         InputProps={{
-          endAdornment: <InputAdornment position="end">Kg</InputAdornment>,
+          endAdornment: <InputAdornment position="end">Lbs</InputAdornment>,
         }}
       />
-      <TextField
+          {/* <TextField
         id="outlined-adornment-password"
         className={clsx(classes.margin, classes.textField)}
         variant="outlined"
@@ -126,7 +169,19 @@ export function Create() {
             </InputAdornment>
           ),
         }}
-      />
-    </div>
-  );
+      /> */}
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Invoice"
+              className="btn-lg"
+              style={{ color: "white", backgroundColor: "#256331" }}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default withStyles(styles)(CreateInvoice);
