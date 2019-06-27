3bb3dbdc71878fc685608a580ca56e45be20854a
Austin Webb <stevenaustinwebb@gmail.com>
2019-06-27 11:38:50 -0500

    edited nav

--- src/temp/home.jsx
+++ src/temp/home.jsx
@@ -1,113 +1,129 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import { ListItemLink } from '../components/ListItem';
-import NoteAddIcon from '@material-ui/icons/NoteAdd';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Link from '@material-ui/core/Link';
-
-// eslint-disable-next-line
-import { Create } from '../components/hooks/Create';
-// eslint-disable-next-line
-import { CreateInvoice } from '../components/inventory/Create';
-
-import MaterialUIPickers from '../components/DatePicker';
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import IconButton from "@material-ui/core/IconButton";
+import Link from "@material-ui/core/Link";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListIcon from "@material-ui/icons/List";
+import MailIcon from "@material-ui/icons/Mail";
+import MenuIcon from "@material-ui/icons/Menu";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import NoteAddIcon from "@material-ui/icons/NoteAdd";
+import clsx from "clsx";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { CreateInvoice } from "../components/inventory/Create";
+import InvoiceList from '../components/inventory/List';
+import { ListItemLink } from "../components/ListItem";
+import SimpleTable from '../data/SimpleTable';
+import { OverView } from '../components/OverView';
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    display: "flex"
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+    fontWeight: 'bold',
+  },
+  toolbarr: {
+    flexWrap: 'wrap'
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   menuButton: {
-    marginRight: 36,
+    marginRight: 36
   },
   hide: {
-    display: 'none',
+    display: "none"
   },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    whiteSpace: 'nowrap',
+    whiteSpace: "nowrap"
   },
   drawerOpen: {
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   drawerClose: {
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      duration: theme.transitions.duration.leavingScreen
     }),
-    overflowX: 'hidden',
+    overflowX: "hidden",
     width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1
+    }
   },
   toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: "0 8px",
+    ...theme.mixins.toolbar
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
+    padding: theme.spacing(3)
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+    fontWeight: 'bold',
+    fontSize: 14,
+    marginRight: 15
   },
 }));
 
 const routes = [
-    {
-        path: "/create",
-        exact: true,
-        main: () => <CreateInvoice />
-    },
-    {
-      path: "/createhook",
-      exact: true,
-      main: () => <Create />
-    },
-    {
-      path: "/picker",
-      exact: true,
-      main: () => < MaterialUIPickers />
-    }
-]
+  {
+    path: "/",
+    exact: true,
+    main: () => <OverView />
+  },
+  {
+    path: "/create",
+    exact: true,
+    main: () => <CreateInvoice />
+  },
+  {
+    path: "/invoices",
+    exact: true,
+    main: () => <SimpleTable />
+  },
+  {
+    path: "/oldinvoices",
+    exact: true,
+    main: () => <InvoiceList />
+  }
+];
 
 export default function MiniDrawer() {
   const classes = useStyles();
@@ -123,88 +139,118 @@ export default function MiniDrawer() {
   }
 
   return (
-      <React.Fragment>
-          <Router>
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="Open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
+    <React.Fragment>
+      <Router>
+        <div className={classes.root}>
+          <CssBaseline />
+          <AppBar
+            position="fixed"
+            className={clsx(classes.appBar, {
+              [classes.appBarShift]: open
             })}
           >
-            <MenuIcon />
-          </IconButton>
-          <Link 
-            variant="h6"
-            color="inherit"
-            href="/"
-            fontWeight='bold'
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                className={clsx(classes.menuButton, {
+                  [classes.hide]: open
+                })}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Link variant="h6" color="inherit" href="/" className={classes.toolbarTitle} >
+                Johnson Sawmill
+              </Link>
+              <nav>
+                <Link
+                  variant="button"
+                  color="inherit"
+                  href="/inventory"
+                  className={classes.link}
+                >
+                  Inventory
+                </Link>
+                <Link
+                  variant="button"
+                  color="inherit"
+                  href="/freight"
+                  className={classes.link}
+                >
+                  Freight
+                </Link>
+                <Link
+                  variant="button"
+                  color="inherit"
+                  href="#"
+                  className={classes.link}
+                >
+                  Records
+                </Link>
+              </nav>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            variant="permanent"
+            className={clsx(classes.drawer, {
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open
+            })}
+            classes={{
+              paper: clsx({
+                [classes.drawerOpen]: open,
+                [classes.drawerClose]: !open
+              })
+            }}
+            open={open}
           >
-              Reckon 
-          </Link>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
-        }}
-        open={open}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-        <List component="nav">
-            <ListItemLink
+            <div className={classes.toolbar}>
+              <IconButton onClick={handleDrawerClose}>
+                {theme.direction === "rtl" ? (
+                  <ChevronRightIcon />
+                ) : (
+                  <ChevronLeftIcon />
+                )}
+              </IconButton>
+            </div>
+            <Divider />
+            <List component="nav">
+              <ListItemLink
                 to="/create"
                 primary="Create Invoice"
-                icon={<NoteAddIcon/>}
-            />
-            <ListItemLink
-                to="/createhook"
-                primary="Create Hook"
-                icon={<NoteAddIcon/>}
-            />
-            <ListItemLink
-                to="/picker"
-                primary="Create Hook"
-                icon={<NoteAddIcon/>}
-            />
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        {routes.map((route, index) => (
+                icon={<NoteAddIcon fontSize="large" />}
+              />
+              <ListItemLink
+                to="/invoices"
+                primary="Invoices"
+                icon={<ListIcon fontSize="large" />}
+              />
+              <ListItemLink
+                to="/oldinvoices"
+                primary="Old Invoices"
+                icon={<ListIcon fontSize="large" />}
+              />
+            </List>
+
+            <Divider />
+
+            <List>
+              {["All mail", "Trash", "Spam"].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            </List>
+          </Drawer>
+
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            {routes.map((route, index) => (
               <Route
                 key={index}
                 path={route.path}
@@ -212,9 +258,10 @@ export default function MiniDrawer() {
                 component={route.main}
               />
             ))}
-      </main>
-    </div>
-    </Router>
+          </main>
+
+        </div>
+      </Router>
     </React.Fragment>
   );
 }
