9b46f6e2c4e1d760f15d87fc0bd936ca653b5d88
Austin Webb <stevenaustinwebb@gmail.com>
2019-07-11 16:08:57 -0500

    added agg

--- src/server/routes/invoice.routes.js
+++ src/server/routes/invoice.routes.js
@@ -1,22 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-let Invoice = require('../models/invoice.model');
+let Invoice = require("../models/invoice.model");
 
-router.route('/add').post(function(req, res) {
-    let invoice = new Invoice(req.body);
-    invoice.save()
-        .then(invoice => {
-            res.status(200).json({'invoice': 'invoice added successfully'});
-        })
-        .catch(err => {
-            res.status(400).send('adding new invoice failed');
-        });
+router.route("/add").post(function(req, res) {
+  let invoice = new Invoice(req.body);
+  invoice
+    .save()
+    .then(invoice => {
+      res.status(200).json({ invoice: "invoice added successfully" });
+    })
+    .catch(err => {
+      res.status(400).send("adding new invoice failed");
+    });
 });
 
-router.get('/', async (req, res) => {
-    const invoices = await Invoice.find({});
-    res.json(invoices);
+router.get("/", async (req, res) => {
+  const invoices = await Invoice.find({});
+  res.json(invoices);
+});
+
+router.get('/total', async (req, res) => {
+  Invoice.aggregate([
+    {
+      $match: {
+        logger_name: 'chip johnson'
+      }
+    }
+  ], function(err, results) {
+    if (err) {
+      res.json(500, err);
+    } else {    
+      res.json({results: results});
+    }
+  })
+
 })
 
 module.exports = router;
