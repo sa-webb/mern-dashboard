20783a3115fb2a9edebd2b19077eab3b25fb8b6c
Austin Webb <stevenaustinwebb@gmail.com>
2019-07-14 15:53:21 -0500

    edit component

---
+++ src/components/inventory/Edit.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+/**
+ * Needs to get by id route 
+ * User can edit the values
+ * Submit using the update route 
+ */
+
+export default function Edit() {
+    return (
+        <p>Success!</p>
+    )
+}
