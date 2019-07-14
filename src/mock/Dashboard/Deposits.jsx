f66e791a9bf8665ccca1f191fc97f0d18052a425
Austin Webb <stevenaustinwebb@gmail.com>
2019-07-14 15:52:31 -0500

    reqs and aggs

--- src/mock/Dashboard/Deposits.jsx
+++ src/mock/Dashboard/Deposits.jsx
@@ -1,29 +1,59 @@
-import React from 'react';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Title from './Title';
+import React, { useEffect, useState } from "react";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Title from "./Title";
 
 const useStyles = makeStyles({
   depositContext: {
-    flex: 1,
+    flex: 1
   },
   spacing: {
     marginTop: 10
   }
 });
 
+const TotalLoads = () => {
+  const classes = useStyles();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/invoices/total")
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  return data.map(number => (
+    <Typography className={classes.spacing} component="p" variant="h4">
+    Total Loads: {number.count}
+    </Typography>
+  ));
+};
+
+const TotalTons = () => {
+  const classes = useStyles();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/invoices/tons")
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  console.log(data);
+
+  return data.map(count => (
+    <Typography className={classes.spacing} component="p" variant="h4">
+    {count.total} Tons
+    </Typography>
+  ));
+};
+
 export default function Deposits() {
   const classes = useStyles();
   return (
     <React.Fragment>
       <Title>Log Purchases</Title>
-      <Typography className={classes.spacing} component="p" variant="h4">
-        $4,629.60
-      </Typography>
-      <Typography className={classes.spacing} component="p" variant="h4">
-        68.91 Tons
-      </Typography>
+      <TotalLoads />
+      <TotalTons />
       <Typography color="textSecondary" className={classes.depositContext}>
         15 July, 2019
       </Typography>
