428985371e9a58992e57bdc051ed3f301397e87f
Austin Webb <stevenaustinwebb@gmail.com>
2019-07-15 07:13:08 -0500

    added summary cards

---
+++ src/components/inventory/summary/Mixed.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Title from "../../helpers/CardTitle";
+
+const useStyles = makeStyles({
+  depositContext: {
+    flex: 1
+  },
+  spacing: {
+    marginTop: 10
+  }
+});
+
+const TotalLoads = () => {
+  const classes = useStyles();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/invoices/mixedtotal")
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  return data.map(number => (
+    <Typography className={classes.spacing} component="p" variant="h4">
+    $ {number.result}
+    </Typography>
+  ));
+};
+
+const TotalTons = () => {
+  const classes = useStyles();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/invoices/mixed")
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  console.log(data);
+
+  return data.map(count => (
+    <Typography className={classes.spacing} component="p" variant="h4">
+    {count.result} Tons
+    </Typography>
+  ));
+};
+
+export default function Mixed(props) {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>{props.title}</Title>
+      <TotalLoads />
+      <TotalTons />
+      <Typography color="textSecondary" className={classes.depositContext}>
+
+      </Typography>
+      <div>
+        <Link color="primary" href="/">
+          View Mixed
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+}
