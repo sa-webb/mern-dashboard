dfebfb0b24be0faa776cfc752dc2f1b823d9dedf
Austin Webb <stevenaustinwebb@gmail.com>
2019-07-19 19:20:41 -0400

    decoupled routes

--- src/constants/inventory.routes.js
+++ src/constants/inventory.routes.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import { CreateInvoice } from '../components/inventory/Create';
-import Fetch from '../components/inventory/Fetch';
+import { CreateInvoice } from "../components/inventory/Create";
+import { Overview } from '../components/inventory/Overview';
+import Fetch from "../components/inventory/Fetch";
+
 
 export const routes = [
-    {
-      path: "/inventory/create",
-      exact: true,
-      main: () => <CreateInvoice />
-    },
-    {
-      path: "/inventory/invoices",
-      exact: true,
-      main: () => <Fetch/>
-    },
-]
+  {
+    path: "/inventory",
+    exact: true,
+    main: () => <Overview />
+  },
+  {
+    path: "/inventory/create",
+    exact: true,
+    main: () => <CreateInvoice />
+  },
+  {
+    path: "/inventory/invoices",
+    exact: true,
+    main: () => <Fetch />
+  }
+];
