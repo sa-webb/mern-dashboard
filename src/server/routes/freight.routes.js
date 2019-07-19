df7f77b5a03db639048faa8f2e599b18cd111c1c
Austin Webb <stevenaustinwebb@gmail.com>
2019-07-19 19:22:10 -0400

    decoupled controller

--- src/server/routes/freight.routes.js
+++ src/server/routes/freight.routes.js
@@ -1,23 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-let Freight = require("../models/freight.model");
+const freight_controller = require("../controllers/freight.controller");
 
-router.route("/add").post(function(req, res) {
-    let freight = new Freight(req.body);
-    freight
-        .save()
-        .then(freight => {
-            res.status(200).json({ freight: "added successfully"});
-        })
-        .catch(err => {
-            res.status(400).send("new freight failed");
-        });
-});
+router.get('/', freight_controller.freight_get_all);
 
-router.get("/", async (req, res) => {
-    const loads = await Freight.find({});
-    res.json(loads);
-});
+router.post('/add', freight_controller.freight_add);
+
+router.get('/count', freight_controller.freight_recent_count);
 
 module.exports = router;
